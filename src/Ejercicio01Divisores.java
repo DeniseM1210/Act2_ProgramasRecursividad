5a940c5e76c81c4f769377c7322098a1b5861cda
Denise Morales Escobedo <denisemorales8764@gmail.com>
2020-10-09 12:44:39 -0500

    Se terminó el programa, se hicieron pruebas y se corrigieron errores

--- src/Ejercicio01Divisores.java
+++ src/Ejercicio01Divisores.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
+class Recursividad{
+	
+	public static void divisoresRecursivo(int num, int num2) {
+		if(num > 0 && num2 % num == 0) {
+			System.out.println("El número '" + num + "' es divisor");
+		}
+		if(num > 0) {
+			divisoresRecursivo(num-1, num2);
+		}
+	}
+}
 
 public class Ejercicio01Divisores {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		
+		int num = 0;
+		int num2 = 0;
+		
+		do {
+			System.out.println("Ingrese un número: ");
+			num = entrada.nextInt();
+			
+		}while(num <= 0);
+		num2 = num;
+		Recursividad.divisoresRecursivo(num, num2);
+		
 
 	}
 
